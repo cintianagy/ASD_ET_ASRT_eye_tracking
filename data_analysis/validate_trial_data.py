c972c9313e45b507f85549a31308eacf71aea52a
Tamás Zolnai <zolnaitamas2000@gmail.com>
2023-04-13 14:25:27 +0200

    Update trial level data validation script.

--- data_analysis/validate_trial_data.py
+++ data_analysis/validate_trial_data.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#    Copyright (C) <2019-2021>  <Tamás Zolnai>  <zolnaitamas2000@gmail.com>
+#    Copyright (C) <2019-2023>  <Tamás Zolnai>  <zolnaitamas2000@gmail.com>
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -21,51 +21,50 @@ import sys
 import pandas
 from utils import strToFloat, floatToStr
 
-def checkRTOutput(trial_file_name, RT_upper_limits):
+def calcRTUpperLimits(raw_file_name):
+    input_data = pandas.read_csv(raw_file_name, sep='\t',
+                                 usecols=['block', 'trial', 'gaze_data_time_stamp'])
+
+    last_trial = 1
+    start_time = 0
+    RT_data = []
+    previous_row = -1
+    start_time_set = False
+
+    for index, row in input_data.iterrows():
+        if not start_time_set and row['trial'] == 1 and row['block'] == 1:
+            start_time = int(row['gaze_data_time_stamp'])
+            start_time_set = True
+
+        # we are at the end of the trial (actually at the first row of the next trial)
+        if last_trial != row['trial'] or index == len(input_data.index) - 1:
+            if isinstance(previous_row, pandas.Series) and str(previous_row['block']) != "0": # validation blocks
+                if index == len(input_data.index) - 1:
+                    end_time = row['gaze_data_time_stamp']
+                else:
+                    end_time = previous_row['gaze_data_time_stamp']
+                RT_data.append((end_time - start_time) / 1000.0)
+
+                last_trial = row['trial']
+                start_time = previous_row['gaze_data_time_stamp']
+
+        previous_row = row
+
+    # 85 trial * 5 block * 8 epoch
+    assert(len(RT_data) == 3400)
+    return RT_data
+
+def validateRTOutput(trial_file_name, RT_upper_limits):
     trial_data = pandas.read_csv(trial_file_name, sep='\t')
 
     assert(len(trial_data.index) == len(RT_upper_limits))
 
     for index, row in trial_data.iterrows():
         actual_RT = strToFloat(row["RT (ms)"])
-        assert(actual_RT < (RT_upper_limits[index] - 350.0))
+        assert(actual_RT < (RT_upper_limits[index] - 380.0))
 
-def calcRTUpperLimits(raw_file_name):
-    input_data = pandas.read_csv(raw_file_name, sep='\t')
-
-    last_trial = "1"
-    start_time = 0
-    start_time_set = False
-    RT_data = []
-    previous_row = -1
-
-    for index, row in input_data.iterrows():
-        if not start_time_set and str(row['trial']) == "1" and str(row['block']) == "1":
-            start_time_set = True
-            start_time = int(row['gaze_data_time_stamp'])
-
-        # we are at the end of the trial (actually at the first row of the next trial)
-        if last_trial != str(row['trial']) or index == len(input_data.index) - 1:
-            if isinstance(previous_row, pandas.Series) and str(previous_row['block']) != "0": # validation blocks
-                end_time = int(previous_row['gaze_data_time_stamp'])
-                RT_data.append((end_time - start_time) / 1000.0)
-
-                last_trial = str(row['trial'])
-                start_time = int(row['gaze_data_time_stamp'])
-
-        previous_row = row
-
-    assert(len(RT_data) == 3280)
-    return RT_data
-
-def getAOI(row):
-    left_gaze_validity = bool(row['left_gaze_validity'])
-    right_gaze_validity = bool(row['right_gaze_validity'])
-
-    left_gaze_X = strToFloat(row['left_gaze_data_X_ADCS'])
-    left_gaze_Y = strToFloat(row['left_gaze_data_Y_ADCS'])
-    right_gaze_X = strToFloat(row['right_gaze_data_X_ADCS'])
-    right_gaze_Y = strToFloat(row['right_gaze_data_Y_ADCS'])
+def getAOI(left_gaze_validity, right_gaze_validity, left_gaze_X, left_gaze_Y,
+           right_gaze_X, right_gaze_Y):
 
     if left_gaze_validity and right_gaze_validity:
         X = (left_gaze_X + right_gaze_X) / 2.0
@@ -77,59 +76,75 @@ def getAOI(row):
         X = right_gaze_X
         Y = right_gaze_Y
     else:
-        return [-1, -1, -1]
+        return -1
 
-    if X <= 0.5 and Y <= 0.5:
-        return [1, X, Y]
-    elif X >= 0.5 and Y <= 0.5:
-        return [2, X, Y]
-    elif X <= 0.5 and Y >= 0.5:
-        return [3, X, Y]
+    if X <= 0.0 and Y >= 0.0:
+        return 1
+    elif X >= 0.0 and Y >= 0.0:
+        return 2
+    elif X <= 0.0 and Y <= 0.0:
+        return 3
     else:
-        return [4, X, Y]
+        return 4
 
-def calcLastAOIGuess(raw_file_name):
-    input_data = pandas.read_csv(raw_file_name, sep='\t')
+def calcLastAOIs(raw_file_name):
+    data_table = pandas.read_csv(raw_file_name, sep='\t',
+                                usecols=['block', 'trial', 'trial_phase', 'left_gaze_validity', 'right_gaze_validity',
+                                         'left_gaze_data_X_PCMCS', 'left_gaze_data_Y_PCMCS', 'right_gaze_data_X_PCMCS', 'right_gaze_data_Y_PCMCS'])
 
-    last_trial = "1"
-    last_ROI_data = []
-    last_ROI_trio = [-1, -1, -1]
-    previous_row = -1
-    last_ROI_found = False
+    anticipation_data = []
+    trial_column = data_table["trial"]
+    block_column = data_table["block"]
+    trial_phase_column = data_table["trial_phase"]
+    left_gaze_validity_column = data_table["left_gaze_validity"]
+    right_gaze_validity_column = data_table["right_gaze_validity"]
+    left_gaze_data_X_column = data_table["left_gaze_data_X_PCMCS"]
+    left_gaze_data_Y_column = data_table["left_gaze_data_Y_PCMCS"]
+    right_gaze_data_X_column = data_table["right_gaze_data_X_PCMCS"]
+    right_gaze_data_Y_column = data_table["right_gaze_data_Y_PCMCS"]
 
-    for index, row in input_data.iterrows():
-        if str(row['block']) == "0": # validation blocks
-            previous_row = row
-            continue
-
-        if not last_ROI_found:
-            if row['trial_phase'] != 'before_stimulus':
-                if str(row['trial']) in ["1", "2"]:
-                    last_ROI_found = True
-                    last_ROI_trio = [-1, -1, -1]
+    last_AOI = -1
+    last_AOI_found = False
+    for i in range(len(trial_column) - 2, -1, -1):
+        # we reached the next trial's last data (we compute the previous trial's last visited AOI).
+        if i == 0:
+            reached_next_trial = False
+        else:
+            reached_next_trial = trial_column[i] != trial_column[i + 1]
+        if reached_next_trial:
+            # we don't compute last AOI data for 0 indexed blocks, which are calibration validation blocks.
+            if str(block_column[i + 1]) != "0":
+                if last_AOI == -1:
+                    anticipation_data.append('none')
                 else:
-                    last_ROI_found = True
-                    prev_AOI = getAOI(previous_row)
-                    current_AOI = getAOI(row)
-                    last_ROI_trio = [prev_AOI[0], current_AOI[1], current_AOI[2]]
+                    anticipation_data.append(last_AOI)
+            last_AOI = -1
+            last_AOI_found = False
 
-        # we are at the end of the trial (actually at the first row of the next trial)
-        if last_trial != str(row['trial']) or index == len(input_data.index) - 1:
-            if isinstance(previous_row, pandas.Series):
-                if last_ROI_found:
-                    last_ROI_data.append(last_ROI_trio)
+        # get AOI during RSI
+        if trial_phase_column[i] == 'before_stimulus' and not last_AOI_found:
+            current_AOI = getAOI(bool(left_gaze_validity_column[i]), bool(right_gaze_validity_column[i]),
+                                 strToFloat(left_gaze_data_X_column[i]), strToFloat(left_gaze_data_Y_column[i]),
+                                 strToFloat(right_gaze_data_X_column[i]), strToFloat(right_gaze_data_Y_column[i]))
+            if current_AOI != -1:
+                last_AOI = current_AOI
+                last_AOI_found = True
+
+        # we need to handle the last trial differently at the end of the data file.
+        if (i == 0):
+            if str(block_column[i + 1]) != "0":
+                if last_AOI == -1:
+                    anticipation_data.append('none')
                 else:
-                    last_ROI_data.append([-1, -1, -1])
-                last_trial = str(row['trial'])
-                last_ROI_found = False
+                    anticipation_data.append(last_AOI)
+            last_AOI = -1
 
-        previous_row = row
+    anticipation_data = anticipation_data[::-1]
+    return anticipation_data
 
-    assert(len(last_ROI_data) == 3280)
-    return last_ROI_data
-
-def checkAOIOutput(trial_file_name, last_AOIs):
+def validateLastAOIs(trial_file_name, last_AOIs):
     trial_data = pandas.read_csv(trial_file_name, sep='\t')
+
 
     assert(len(trial_data.index) == len(last_AOIs))
 
@@ -137,36 +152,11 @@ def checkAOIOutput(trial_file_name, last_AOIs):
     failed_items = 0
     for index, row in trial_data.iterrows():
         actual_last_AOI = row["last_AOI_before_stimulus"]
-        if str(row["trial"]) in ["1", "2"]:
-            assert(actual_last_AOI == "none")
-            checked_items += 1
-        elif last_AOIs[index][0] != -1:
-            if actual_last_AOI != str(last_AOIs[index][0]):
-                assert(abs(last_AOIs[index][1] - 0.5) < 0.05 or
-                       abs(last_AOIs[index][2] - 0.5) < 0.05 ) # close to AOI boundaries
-            checked_items += 1
-
-    print("Last AOI validation. Validated items: " + str(checked_items))
-
-def checkAOIForNullRTs(trial_file_name):
-    trial_data = pandas.read_csv(trial_file_name, sep='\t')
-
-    checked_items = 0
-    for index, row in trial_data.iterrows():
-        actual_last_AOI = row["last_AOI_before_stimulus"]
-        if str(row["trial"]) in ["1", "2"]:
-            assert(actual_last_AOI == "none")
-            checked_items += 1
-        elif str(row["RT (ms)"]) == "0":
-            checked_items += 1
-            assert(str(row["stimulus"]) == actual_last_AOI)
-
-    print("Last AOI validation for 0 RTs. Validated items: " + str(checked_items))
+        assert(actual_last_AOI == last_AOIs[index])
 
 def validateTrialData(raw_file_name, trial_file_name):
     RT_upper_limits = calcRTUpperLimits(raw_file_name)
-    checkRTOutput(trial_file_name, RT_upper_limits)
+    validateRTOutput(trial_file_name, RT_upper_limits)
 
-    last_AOIs = calcLastAOIGuess(raw_file_name)
-    checkAOIOutput(trial_file_name, last_AOIs)
-    checkAOIForNullRTs(trial_file_name)
+    last_AOIs = calcLastAOIs(raw_file_name)
+    validateLastAOIs(trial_file_name, last_AOIs)
